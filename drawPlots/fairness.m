f148f9e8561b1f68254d00d444b50470e7a35fcb
roamiri <amiriruhollah@gmail.com>
2018-06-16 10:45:03 -0600

    working on sim results

--- drawPlots/fairness.m
+++ drawPlots/fairness.m
@@ -11,7 +11,7 @@ filename = strcat(s);
 load(filename);
 
 %%
-figure;
+% figure;
 hold on;
 grid on;
 box on;
@@ -19,8 +19,8 @@ box on;
 fariness = zeros(1,16);
 fariness_ref = zeros(1,16);
 for i=1:16
-vec = C_FUE_Mat{i};
-vec_ref = C_FUE_Mat_ref{i};
+vec = C_FUE_Mat_4{i};
+% vec_ref = C_FUE_Mat_ref{i};
 num = 0.0;
 num_ref = 0.0;
 denom = 0.0;
@@ -28,14 +28,14 @@ denom_ref = 0.0;
 n = size(vec,2);
 for j=1:n
     num = num + vec(j);
-    num_ref = num_ref + vec_ref(j);
+%     num_ref = num_ref + vec_ref(j);
     denom = denom + vec(j)^2;
-    denom_ref = denom_ref + vec_ref(j)^2;
+%     denom_ref = denom_ref + vec_ref(j)^2;
 end
     fariness(i) = (num^2)/(n*denom);
-    fairness_ref(i) = (num_ref^2)/(n*denom_ref);
+%     fairness_ref(i) = (num_ref^2)/(n*denom_ref);
 end
-plot(fariness, 'r--.', 'LineWidth',1,'MarkerSize',10);
+plot(fariness, 'k--.', 'LineWidth',1,'MarkerSize',10);
 % plot(fairness_ref, 'b--.', 'LineWidth',1,'MarkerSize',10);
 xlim([2 15]);
 ylim([0 1.05]);
--- drawPlots/meanOverResults.m
+++ drawPlots/meanOverResults.m
@@ -43,12 +43,12 @@ for i=1:16
     min_FUE = [min_FUE min(C_FUE_Mat{i})];
 end
 %%
-MUE_C_CL = [];    
-min_FUE_CL = [];
-sum_FUE_CL = [];
-max_MUE_CL = [];
-max_FUE_CL = [];
-C_FUE_Mat_CL = cell(1,16);
+MUE_C_1 = [];    
+min_FUE_1 = [];
+sum_FUE_1 = [];
+max_MUE_1 = [];
+max_FUE_1 = [];
+C_FUE_Mat_1 = cell(1,16);
 for i=1:16
     fprintf('FBS num = %d\t', i);
     maxmue = 0.;
@@ -62,7 +62,7 @@ for i=1:16
     
     for j=1:100
 %         s = sprintf('Rref_1/R3_%d_%d.mat',i,j);
-        s = sprintf('Jun13/SO_1/pro_CL_%d_%d.mat',i,j);
+        s = sprintf('Jun14/learn_rate/pro_IL_1_%d_%d.mat',i,j);
         filename = strcat(s);
         if exist(s)
             load(filename);
@@ -75,20 +75,20 @@ for i=1:16
         end
     end
     fprintf('Total Cnt = %d\n',Cnt);
-    max_MUE_CL = [max_MUE_CL maxmue];
-    max_FUE_CL = [max_FUE_CL maxfue];
-    MUE_C_CL = [MUE_C_CL mue_C/Cnt]; 
-    sum_FUE_CL = [sum_FUE_CL sumfue/Cnt];
-    C_FUE_Mat_CL{i} = c_fue_vec./Cnt;
-    min_FUE_CL = [min_FUE_CL min(C_FUE_Mat_CL{i})];
+    max_MUE_1 = [max_MUE_1 maxmue];
+    max_FUE_1 = [max_FUE_1 maxfue];
+    MUE_C_1 = [MUE_C_1 mue_C/Cnt]; 
+    sum_FUE_1 = [sum_FUE_1 sumfue/Cnt];
+    C_FUE_Mat_1{i} = c_fue_vec./Cnt;
+    min_FUE_1 = [min_FUE_1 min(C_FUE_Mat_1{i})];
 end
 %%
-MUE_C_ILQ = [];    
-min_FUE_ILQ = [];
-sum_FUE_ILQ = [];
-max_MUE_ILQ = [];
-max_FUE_ILQ = [];
-C_FUE_Mat_ILQ = cell(1,16);
+MUE_C_2 = [];    
+min_FUE_2 = [];
+sum_FUE_2 = [];
+max_MUE_2 = [];
+max_FUE_2 = [];
+C_FUE_Mat_2 = cell(1,16);
 for i=1:16
     fprintf('FBS num = %d\t', i);
     maxmue = 0.;
@@ -102,7 +102,7 @@ for i=1:16
     
     for j=1:100
 %         s = sprintf('Rref_1/R3_%d_%d.mat',i,j);
-        s = sprintf('Jun14/state/pro_IL_S32_%d_%d.mat',i,j);
+        s = sprintf('Jun14/learn_rate/pro_IL_77_%d_%d.mat',i,j);
         filename = strcat(s);
         if exist(s)
             load(filename);
@@ -115,12 +115,52 @@ for i=1:16
         end
     end
     fprintf('Total Cnt = %d\n',Cnt);
-    max_MUE_ILQ = [max_MUE_ILQ maxmue];
-    max_FUE_ILQ = [max_FUE_ILQ maxfue];
-    MUE_C_ILQ = [MUE_C_ILQ mue_C/Cnt]; 
-    sum_FUE_ILQ = [sum_FUE_ILQ sumfue/Cnt];
-    C_FUE_Mat_ILQ{i} = c_fue_vec./Cnt;
-    min_FUE_ILQ = [min_FUE_ILQ min(C_FUE_Mat_ILQ{i})];
+    max_MUE_2 = [max_MUE_2 maxmue];
+    max_FUE_2 = [max_FUE_2 maxfue];
+    MUE_C_2 = [MUE_C_2 mue_C/Cnt]; 
+    sum_FUE_2 = [sum_FUE_2 sumfue/Cnt];
+    C_FUE_Mat_2{i} = c_fue_vec./Cnt;
+    min_FUE_2 = [min_FUE_2 min(C_FUE_Mat_2{i})];
+end
+%%
+MUE_C_3 = [];    
+min_FUE_3 = [];
+sum_FUE_3 = [];
+max_MUE_3 = [];
+max_FUE_3 = [];
+C_FUE_Mat_3 = cell(1,16);
+for i=1:16
+    fprintf('FBS num = %d\t', i);
+    maxmue = 0.;
+    maxfue = 0.;
+    mue_C = 0.;
+    minfue = 0.;
+    sumfue = 0.;
+    c_fue_vec = zeros(1,i);
+    Cnt = 0;
+    lowCnt = 0;
+    
+    for j=1:100
+%         s = sprintf('Rref_1/R3_%d_%d.mat',i,j);
+        s = sprintf('Jun15/ILCL/pro_CL_77_%d_%d.mat',i,j);
+        filename = strcat(s);
+        if exist(s)
+            load(filename);
+%                 C = QFinal.mue.C_profile;
+%                 cc = sum(C(40000:size(C,2)))/(-40000+size(C,2)+1);
+                mue_C = mue_C + QFinal.mue.C;
+                sumfue = sumfue + QFinal.sum_CFUE;
+                c_fue_vec = c_fue_vec + QFinal.C_FUE;
+                Cnt = Cnt+1;
+        end
+    end
+    fprintf('Total Cnt = %d\n',Cnt);
+    max_MUE_3 = [max_MUE_3 maxmue];
+    max_FUE_3 = [max_FUE_3 maxfue];
+    MUE_C_3 = [MUE_C_3 mue_C/Cnt]; 
+    sum_FUE_3 = [sum_FUE_3 sumfue/Cnt];
+    C_FUE_Mat_3{i} = c_fue_vec./Cnt;
+    min_FUE_3 = [min_FUE_3 min(C_FUE_Mat_3{i})];
 end
 %%
 figure;
@@ -129,8 +169,9 @@ grid on;
 box on;
 plot(ones(1,16)*1.0, '--k', 'LineWidth',1);
 plot(MUE_C, '--*r', 'LineWidth',1,'MarkerSize',10);
-plot(MUE_C_CL, '--*b', 'LineWidth',1,'MarkerSize',10);
-% plot(MUE_C_ILQ, '--*g', 'LineWidth',1,'MarkerSize',10);
+plot(MUE_C_1, '--*b', 'LineWidth',1,'MarkerSize',10);
+plot(MUE_C_2, '--*g', 'LineWidth',1,'MarkerSize',10);
+% plot(MUE_C_4, '--*k', 'LineWidth',1,'MarkerSize',10);
 title('MUE capacity','FontSize',14, 'FontWeight','bold');
 xlabel('FBS Numbers','FontSize',14, 'FontWeight','bold');
 ylabel('Capacity(b/s/HZ)','FontSize',14, 'FontWeight','bold');
@@ -145,17 +186,20 @@ box on;
 plot( ones(1,16)*1.0, '--k', 'LineWidth',1);
 for i=1:16
     vec = C_FUE_Mat{i};
-    vec_ref = C_FUE_Mat_CL{i};
-%     vec_ilq = C_FUE_Mat_ILQ{i};
+    vec_ref = C_FUE_Mat_1{i};
+    vec_ilq = C_FUE_Mat_2{i};
+%     vec_4 = C_FUE_Mat_4{i};
     for j=1:size(vec,2)
         plot(i,vec(j), '*r', 'LineWidth',1,'MarkerSize',10);
         plot(i,vec_ref(j), '*b', 'LineWidth',1,'MarkerSize',10);
-%         plot(i,vec_ilq(j), '*g', 'LineWidth',1,'MarkerSize',10);
+        plot(i,vec_ilq(j), '*g', 'LineWidth',1,'MarkerSize',10);
+%         plot(i,vec_4(j), '*k', 'LineWidth',1,'MarkerSize',10);
     end
 end
 plot(min_FUE, '--r', 'LineWidth',1,'MarkerSize',10);
-plot(min_FUE_CL, '--b', 'LineWidth',1,'MarkerSize',10);
-% plot(min_FUE_ILQ, '--g', 'LineWidth',1,'MarkerSize',10);
+plot(min_FUE_1, '--b', 'LineWidth',1,'MarkerSize',10);
+plot(min_FUE_2, '--g', 'LineWidth',1,'MarkerSize',10);
+% plot(min_FUE_4, '--k', 'LineWidth',1,'MarkerSize',10);
 title('FUEs capacity','FontSize',14, 'FontWeight','bold');
 xlabel('FBS Numbers','FontSize',14, 'FontWeight','bold');
 ylabel('Capacity(b/s/HZ)','FontSize',14, 'FontWeight','bold');
@@ -169,8 +213,9 @@ grid on;
 box on;
 % plot( ones(1,16)*2.0, '--k', 'LineWidth',1 );
 plot(sum_FUE, '--*r', 'LineWidth',1,'MarkerSize',10);
-plot(sum_FUE_CL, '--*b', 'LineWidth',1,'MarkerSize',10);
-% plot(sum_FUE_ILQ, '--*g', 'LineWidth',1,'MarkerSize',10);
+plot(sum_FUE_1, '--*b', 'LineWidth',1,'MarkerSize',10);
+plot(sum_FUE_2, '--*g', 'LineWidth',1,'MarkerSize',10);
+% plot(sum_FUE_4, '--*k', 'LineWidth',1,'MarkerSize',10);
 title('SUM capacity of FUEs','FontSize',14, 'FontWeight','bold');
 xlabel('FBS Numbers','FontSize',14, 'FontWeight','bold');
 ylabel('Capacity(b/s/HZ)','FontSize',14, 'FontWeight','bold');
