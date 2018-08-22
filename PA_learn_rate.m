6d9eeac414f327fdca8031e182d263e53d8a56a6
roamiri <amiriruhollah@gmail.com>
2018-08-22 13:21:09 -0600

    test for CL

--- PA_learn_rate.m
+++ PA_learn_rate.m
@@ -204,6 +204,6 @@ FBS{j} = fbs;
     answer.episode = episode;
     answer.time = toc(tt);
     QFinal = answer;
-    save(sprintf('Aug22/T1/pro_x_3_IL_%d_%d.mat', fbsCount, saveNum),'QFinal');
+    save(sprintf('Aug22/T1/pro_x_3_CL_%d_%d.mat', fbsCount, saveNum),'QFinal');
     FBS_out = FBS;
 end
