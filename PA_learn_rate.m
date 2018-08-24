764bf2813f0949320bffca505766b82164b1bd1d
roamiri <amiriruhollah@gmail.com>
2018-08-24 16:13:12 -0600

    debug

--- PA_learn_rate.m
+++ PA_learn_rate.m
@@ -205,6 +205,6 @@ FBS{j} = fbs;
     answer.episode = episode;
     answer.time = toc(tt);
     QFinal = answer;
-    save(sprintf('Aug23/T1/pro_x_1_CL_%d_%d.mat', fbsCount, saveNum),'QFinal');
+    save(sprintf('Aug22/T1/pro_x_1_CL_%d_%d.mat', fbsCount, saveNum),'QFinal');
     FBS_out = FBS;
 end
