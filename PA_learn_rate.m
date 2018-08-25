72539d82bf5c0222ab4c44ec8a2f295001f762e4
roamiri <amiriruhollah@gmail.com>
2018-08-25 09:04:48 -0600

    x4, IL, 5000

--- PA_learn_rate.m
+++ PA_learn_rate.m
@@ -135,9 +135,9 @@ FBS{j} = fbs;
             if mue.C < q_mue, I_mue = 0; else, I_mue = 1; end
             if fbs.C_FUE < q_fue, I_fue = 0; else, I_fue=1; end
 %             fbs.s_new = 16*I_fue + fbs.index; % state set X_2
-            fbs.s_new = 16*I_mue + fbs.index; % state set X_3
+%            fbs.s_new = 16*I_mue + fbs.index; % state set X_3
 %             fbs.s_new = fbs.index; % state set X_1
-%             fbs.s_new = 32*I_mue+16*I_fue + fbs.index; % state set X_4
+             fbs.s_new = 32*I_mue+16*I_fue + fbs.index; % state set X_4
 %             if mue.C <= q_mue
 %                 if (fbs.s_index>16), fbs.s_new = fbs.s_index-16; else, fbs.s_new = fbs.s_index; end
 %             else
@@ -205,6 +205,6 @@ FBS{j} = fbs;
     answer.episode = episode;
     answer.time = toc(tt);
     QFinal = answer;
-    save(sprintf('Aug22/T1/pro_x_3_CL_%d_%d.mat', fbsCount, saveNum),'QFinal');
+    save(sprintf('Aug22/T1/pro_x_4_IL_%d_%d.mat', fbsCount, saveNum),'QFinal');
     FBS_out = FBS;
 end
