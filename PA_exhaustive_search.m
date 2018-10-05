296f48f8c8c6b398f2d06eb8cc6170bf4c6c17ee
roamiri <amiriruhollah@gmail.com>
2018-10-04 22:14:00 -0600

    test for 10 FBSs and exhaustive search

--- PA_exhaustive_search.m
+++ PA_exhaustive_search.m
@@ -5,7 +5,7 @@
 
 function FBS_out = PA_exhaustive_search( FBS_in, MBS, mue, Npower, fbsCount, femtocellPermutation, NumRealization, saveNum, kk)
 
-if fbsCount<9
+if fbsCount<10
     FBS_out = FBS_in;
     return;
 end
@@ -44,22 +44,25 @@ for j=1:11
     p8 = actions(j);
     for jj=1:11
         p9 = actions(jj);
-        fprintf('mini = %d ', j*jj);
-        for i = 1:size(all_actions,1)
-            p_ar = [all_actions(i,:) p8 p9];
+        for jjj=1:11
+            p10 = actions(jjj);
+            fprintf('mini = %d ', j*jj*jjj);
+            for i = 1:size(all_actions,1)
+                p_ar = [all_actions(i,:) p8 p9 p10];
 
-            % calc FUEs and MUEs transmission rate
-            SINR_FUE_Vec = SINR_FUE_3(G, L, K, p_ar, MBS.P, -174);
-            r0 = log2(1+SINR_MUE_5(G, L, K, p_ar, MBS.P, -174));
+                % calc FUEs and MUEs transmission rate
+                SINR_FUE_Vec = SINR_FUE_3(G, L, K, p_ar, MBS.P, -174);
+                r0 = log2(1+SINR_MUE_5(G, L, K, p_ar, MBS.P, -174));
 
-            if r0>=q_mue
-                rate_array = log2(1+SINR_FUE_Vec);
-                rsum = sum(rate_array);
-                if(rsum>Rmax)
-                    R0max = r0;
-                    Rmax = rsum;
-                    Rate_array=rate_array;
-                    best_actions = p_ar;
+                if r0>=q_mue
+                    rate_array = log2(1+SINR_FUE_Vec);
+                    rsum = sum(rate_array);
+                    if(rsum>Rmax)
+                        R0max = r0;
+                        Rmax = rsum;
+                        Rate_array=rate_array;
+                        best_actions = p_ar;
+                    end
                 end
             end
         end
